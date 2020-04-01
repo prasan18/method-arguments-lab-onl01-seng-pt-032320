e10aae0e6303857f1f78578184c16782fb133ff4
ANTONY JUDE PRASAN POOBALARAYAN <prasan29@yahoo.com>
2020-04-01 21:53:55 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,6 +2,5 @@ def introduction(name="josh")
 puts "Hi, my name is #{name}." 
  end
  def introduction_with_language(name,language)
-   puts"Hi, my name is #{name} and I am
-learning to program in #{language}."
+   puts"Hi, my name is #{name} and I am learning to program in #{language}."
 end
