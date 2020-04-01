6c663b030f89ab0b8aa14c2132cd4bd4b0dffb57
ANTONY JUDE PRASAN POOBALARAYAN <prasan29@yahoo.com>
2020-04-01 21:46:51 +0000

    Done.

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,4 +1,3 @@
-def introduction_with_language
-  puts "Hi, my name is #{name}and I am learning to program in #{language}."
-  end
-  learn submit_tag
+def introduction(name="josh")
+puts "Hi, my name is #{name}." 
+ end
